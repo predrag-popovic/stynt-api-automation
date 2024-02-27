fd7626d4dc808fd7c949108ed15a73bdf2588ec7
qatestqa75 <qatestqa75@gmail.com>
2024-02-27 09:46:33 +0100

    Office Login API Test

--- src/test/java/data/login/officeTestData.java
+++ src/test/java/data/login/officeTestData.java
@@ -13,6 +13,12 @@ public class officeTestData {
     public static final  String OFFICE_LOGIN_END_POINT_URL = "https://offices.preprod.stynt.com/app/wapi/v1/login";
     public static final String OFFICE_VERIFIED_EMAIL = "qatestqa75+db1@gmail.com";
     public static final String OFFICE_VERIFIED_PASSWORD = "Gecko123";
+    public static final String OFFICE_EMPTY_EMAIL = "";
+    public static final String OFFICE_EMPTY_PASSWORD = "";
+    public static final String OFFICE_INVALID_EMAIL = "qatestqa75+db12143124pro@gmail.com";
+    public static final String OFFICE_INVALID_PASSWORD = "Gecko12";
+    public static final String OFFICE_UNVERIFIED_EMAIL = "qatestqa75+tos1@gmail.com";
+    public static final String OFFICE_DISABLED_EMAIL = "qatestqa75+db2@gmail.com";
 
     /********************************************************************************************
      METHOD FOR SENDING PARAMETERS FOR CREDENTIALS
@@ -22,6 +28,38 @@ public class officeTestData {
         requestParamsForOfficeSubmitValidCredentials.put("email", OFFICE_VERIFIED_EMAIL);
         requestParamsForOfficeSubmitValidCredentials.put("password", OFFICE_VERIFIED_PASSWORD);
     }
+    public static void ParamsForEmptyCredentials(JSONObject requestParamsForOfficeSubmitEmptyCredentials) {
+        requestParamsForOfficeSubmitEmptyCredentials.put("email", OFFICE_EMPTY_EMAIL);
+        requestParamsForOfficeSubmitEmptyCredentials.put("password", OFFICE_EMPTY_PASSWORD);
+    }
+    public static void ParamsForValidEmailAndEmptyPassword(JSONObject requestParamsForOfficeSubmitValidValidEmailAndEmptyPassword) {
+        requestParamsForOfficeSubmitValidValidEmailAndEmptyPassword.put("email", OFFICE_VERIFIED_EMAIL);
+        requestParamsForOfficeSubmitValidValidEmailAndEmptyPassword.put("password", OFFICE_EMPTY_PASSWORD);
+    }
+    public static void ParamsForEmptyEmailAndValidPassword(JSONObject requestParamsForOfficeSubmitValidValidEmailAndEmptyPassword) {
+        requestParamsForOfficeSubmitValidValidEmailAndEmptyPassword.put("email", OFFICE_EMPTY_EMAIL);
+        requestParamsForOfficeSubmitValidValidEmailAndEmptyPassword.put("password", OFFICE_VERIFIED_PASSWORD);
+    }
+    public static void ParamsForInvalidCredentials(JSONObject requestParamsForOfficeSubmitInvalidCredentials) {
+        requestParamsForOfficeSubmitInvalidCredentials.put("email", OFFICE_INVALID_EMAIL);
+        requestParamsForOfficeSubmitInvalidCredentials.put("password", OFFICE_INVALID_PASSWORD);
+    }
+    public static void ParamsForInvalidEmailAndValidPassword(JSONObject requestParamsForOfficeSubmitInvalidEmailAndValidPassword) {
+        requestParamsForOfficeSubmitInvalidEmailAndValidPassword.put("email", OFFICE_INVALID_EMAIL);
+        requestParamsForOfficeSubmitInvalidEmailAndValidPassword.put("password", OFFICE_VERIFIED_PASSWORD);
+    }
+    public static void ParamsForValidEmailAndInvalidPassword(JSONObject requestParamsForOfficeSubmitValidEmailAndInvalidPassword) {
+        requestParamsForOfficeSubmitValidEmailAndInvalidPassword.put("email", OFFICE_VERIFIED_EMAIL);
+        requestParamsForOfficeSubmitValidEmailAndInvalidPassword.put("password", OFFICE_INVALID_PASSWORD);
+    }
+    public static void ParamsForUnverifiedUserValidCredentials(JSONObject requestParamsForOfficeSubmitValidCredentials) {
+        requestParamsForOfficeSubmitValidCredentials.put("email", OFFICE_UNVERIFIED_EMAIL);
+        requestParamsForOfficeSubmitValidCredentials.put("password", OFFICE_VERIFIED_PASSWORD);
+    }
+    public static void ParamsForDisabledUserValidCredentials(JSONObject requestParamsForOfficeSubmitValidCredentials) {
+        requestParamsForOfficeSubmitValidCredentials.put("email", OFFICE_DISABLED_EMAIL);
+        requestParamsForOfficeSubmitValidCredentials.put("password", OFFICE_VERIFIED_PASSWORD);
+    }
 
     /********************************************************************************************
      METHOD FOR VERIFICATION EXPECTED RESULT THAT WE GET FORM BODY
@@ -29,5 +67,8 @@ public class officeTestData {
     public static void verifyThaOfficeLoginSuccessfully(Response officeSuccessfullyLogin) {
         Assertions.assertEquals(200, officeSuccessfullyLogin.statusCode());
         Assertions.assertEquals("true", officeSuccessfullyLogin.jsonPath().getString("success"));
+    }
+    public static void verifyThatOfficeLoginUnsuccessfully(Response officeUnsuccessfullyLogin) {
+        Assertions.assertEquals(401, officeUnsuccessfullyLogin.statusCode());
     }
 }
--- src/test/java/office_api_tests/officeUserLogin.java
+++ src/test/java/office_api_tests/officeUserLogin.java
@@ -25,7 +25,7 @@ public class officeUserLogin extends officeTestData {
 
     @Test
     @Order(1)
-    @Feature("01. TC Verify That The Verify Office user can successfully login")
+    @Feature("01. TC Verify that the verified Office user can successfully login")
     @Severity(SeverityLevel.BLOCKER)
     public void verifyThatTheVerifyOfficeCanSuccessfullyLogin() {
 
@@ -35,6 +35,110 @@ public class officeUserLogin extends officeTestData {
         Response officeLogin = getResponseOfficeLogin(requestParamsForOfficeSubmitValidCredentials);
 
         verifyThaOfficeLoginSuccessfully(officeLogin);
+    }
+    @Test
+    @Order(2)
+    @Feature("02. TC Verify that verified user can't login with empty email and password")
+    @Severity(SeverityLevel.BLOCKER)
+    public void verifyThatTheVerifiedOfficeCanNotLoginWithEmptyData() {
+
+        JSONObject requestParamsForOfficeSubmitEmptyCredentials = new JSONObject();
+        ParamsForEmptyCredentials(requestParamsForOfficeSubmitEmptyCredentials);
+
+        Response officeLogin = getResponseOfficeLogin(requestParamsForOfficeSubmitEmptyCredentials);
+
+        verifyThatOfficeLoginUnsuccessfully(officeLogin);
+    }
+    @Test
+    @Order(3)
+    @Feature("03. TC Verify that verified user can't login with valid email and empty password")
+    @Severity(SeverityLevel.BLOCKER)
+    public void verifyThatTheVerifiedOfficeCanNotLoginWithValidEmailAndEmptyPassword() {
+
+        JSONObject requestParamsForOfficeSubmitValidEmailAndEmptyPassword = new JSONObject();
+        ParamsForValidEmailAndEmptyPassword(requestParamsForOfficeSubmitValidEmailAndEmptyPassword);
+
+        Response officeLogin = getResponseOfficeLogin(requestParamsForOfficeSubmitValidEmailAndEmptyPassword);
+
+        verifyThatOfficeLoginUnsuccessfully(officeLogin);
+    }
+    @Test
+    @Order(4)
+    @Feature("04. TC Verify that verified user can't login with empty email and valid password")
+    @Severity(SeverityLevel.BLOCKER)
+    public void verifyThatTheVerifiedOfficeCanNotLoginWithEmptyEmailAndValidPassword() {
+
+        JSONObject requestParamsForOfficeSubmitEmptyEmailAndValidPassword = new JSONObject();
+        ParamsForEmptyEmailAndValidPassword(requestParamsForOfficeSubmitEmptyEmailAndValidPassword);
+
+        Response officeLogin = getResponseOfficeLogin(requestParamsForOfficeSubmitEmptyEmailAndValidPassword);
+
+        verifyThatOfficeLoginUnsuccessfully(officeLogin);
+    }
+    @Test
+    @Order(5)
+    @Feature("05. TC Verify that verified user can't login with invalid email and password")
+    @Severity(SeverityLevel.BLOCKER)
+    public void verifyThatTheVerifiedOfficeCanNotLoginWithInvalidData() {
+
+        JSONObject requestParamsForOfficeSubmitInvalidCredentials = new JSONObject();
+        ParamsForInvalidCredentials(requestParamsForOfficeSubmitInvalidCredentials);
+
+        Response officeLogin = getResponseOfficeLogin(requestParamsForOfficeSubmitInvalidCredentials);
+
+        verifyThatOfficeLoginUnsuccessfully(officeLogin);
+    }
+    @Test
+    @Order(6)
+    @Feature("06. TC Verify that verified user can't login with invalid email and valid password")
+    @Severity(SeverityLevel.BLOCKER)
+    public void verifyThatTheVerifiedOfficeCanNotLoginWithInvalidEmailAndValidPassword() {
+
+        JSONObject requestParamsForOfficeSubmitInvalidEmailAndValidPassword = new JSONObject();
+        ParamsForInvalidEmailAndValidPassword(requestParamsForOfficeSubmitInvalidEmailAndValidPassword);
+
+        Response officeLogin = getResponseOfficeLogin(requestParamsForOfficeSubmitInvalidEmailAndValidPassword);
+
+        verifyThatOfficeLoginUnsuccessfully(officeLogin);
+    }
+    @Test
+    @Order(7)
+    @Feature("07. TC Verify that verified user can't login with valid email and invalid password")
+    @Severity(SeverityLevel.BLOCKER)
+    public void verifyThatTheVerifiedOfficeCanNotLoginWithValidEmailAndInvalidPassword() {
+
+        JSONObject requestParamsForOfficeSubmitValidEmailAndInvalidPassword = new JSONObject();
+        ParamsForValidEmailAndInvalidPassword(requestParamsForOfficeSubmitValidEmailAndInvalidPassword);
+
+        Response officeLogin = getResponseOfficeLogin(requestParamsForOfficeSubmitValidEmailAndInvalidPassword);
+
+        verifyThatOfficeLoginUnsuccessfully(officeLogin);
+    }
+    @Test
+    @Order(8)
+    @Feature("08. TC Verify that unverified user can login with valid email and password")
+    @Severity(SeverityLevel.BLOCKER)
+    public void verifyThatTheUnverifiedOfficeCanSuccessfullyLogin() {
+
+        JSONObject requestParamsForOfficeSubmitValidCredentialsForUnverifiedUser = new JSONObject();
+        ParamsForUnverifiedUserValidCredentials(requestParamsForOfficeSubmitValidCredentialsForUnverifiedUser);
+
+        Response officeLogin = getResponseOfficeLogin(requestParamsForOfficeSubmitValidCredentialsForUnverifiedUser);
+
+        verifyThaOfficeLoginSuccessfully(officeLogin);
+    }
+    @Test
+    @Order(9)
+    @Feature("09. TC Verify that the disabled user can login with a valid email and password")
+    @Severity(SeverityLevel.BLOCKER)
+    public void verifyThatTheDisabledOfficeCanSuccessfullyLogin() {
+
+        JSONObject requestParamsForOfficeSubmitValidCredentialsForDisabledUser = new JSONObject();
+        ParamsForDisabledUserValidCredentials(requestParamsForOfficeSubmitValidCredentialsForDisabledUser);
+
+        Response officeLogin = getResponseOfficeLogin(requestParamsForOfficeSubmitValidCredentialsForDisabledUser);
+
+        verifyThatOfficeLoginUnsuccessfully(officeLogin);
     }
 
     private static Response getResponseOfficeLogin(JSONObject requestParams) {
